d7087358093883b629035028e316f635b043cf6a
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-10 03:48:14 +0600

    list screen created

--- first/App.js
+++ first/App.js
@@ -3,16 +3,17 @@ import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
-
+import ListScreen from "./src/screens/ListScreen";
 const stack = createStackNavigator();
 
 function App(){
     return(
-        <NavigationContainer>
-            <stack.Navigator>
-                <stack.Screen name="Home" component={HomeScreen}>
+        <NavigationContainer >
+            <stack.Navigator initialRouteName="Home">
 
-                </stack.Screen>
+                <stack.Screen name="Home" component={HomeScreen}/>
+                <stack.Screen name="List" component={ListScreen}/>
+
             </stack.Navigator>
         </NavigationContainer>
     )
---
+++ first/src/screens/ListScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Text, View, StyleSheet} from "react-native";
+
+
+const ListScreen = () =>{
+    const uni_1 = "IUT";
+    const uni_2 = "BUET";
+    const uni_3 = "DU";
+    const uni_4 = "RUET";
+    const uni_5 = "KUET";
+    const uni_6 = "CUET";
+    const uni_7 = "SUST";
+    const uni_8 = "BRAC";
+    return(
+        <view style={styles.viewStyle}>
+            <Text style={styles.textStyle}>{uni_2}</Text>
+            <Text style={styles.textStyle}>{uni_1}</Text>
+            <Text style={styles.textStyle}>{uni_3}</Text>
+            <Text style={styles.textStyle}>{uni_4}</Text>
+            <Text style={styles.textStyle}>{uni_5}</Text>
+            <Text style={styles.textStyle}>{uni_6}</Text>
+            <Text style={styles.textStyle}>{uni_7}</Text>
+            <Text style={styles.textStyle}>{uni_8}</Text>
+        </view>
+    );
+}
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize:20,
+        color:'#ffffff',
+        marginVertical: 30,
+    },
+    viewStyle: {
+        backgroundColor: "#ff00ff",
+        borderColor: '#ecfd00',
+        borderWidth: 5,
+    }
+})
+
+export default ListScreen;
