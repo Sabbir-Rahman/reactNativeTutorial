8c98090dccb6aef5f459374d4e6b6718c1f1c073
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-12 23:56:14 +0600

    store object with asyncstorage

--- first/src/screens/storage.js
+++ first/src/screens/storage.js
@@ -23,11 +23,36 @@ const StorageScreen = () => {
         }
     };
 
+    const setDataJson = async (key, value)=> {
+        try {
+            value = JSON.stringify(value);
+            await AsyncStorage.setItem(key,value);
+            alert("Data Saved Successfully");
+        } catch (error){
+            alert(error);
+        }
+    };
+
     const getData = async (key)=>{
         try {
             const data = await AsyncStorage.getItem(key);
             if(data != null) {
                 alert(data);
+            }
+            else {
+                alert("No value");
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    const getDataJson = async (key)=>{
+        try {
+            let data = await AsyncStorage.getItem(key);
+            if(data != null) {
+                data = JSON.parse(data);
+                console.log(data);
             }
             else {
                 alert("No value");
@@ -50,15 +75,18 @@ const StorageScreen = () => {
       <View>
           <Text>Storage Screen</Text>
           <Button title="Save Data" onPress={function (){
-              setData("name","Md Sabbir Rahman")
+              //setData("name","Md Sabbir Rahman");
+              setDataJson("StudentInfo",{ID:'180042142',Programme:'SWE', Semester:'4th'})
           }}/>
 
           <Button title="Retrieve Data" onPress={function (){
-              getData("name");
+              //getData("name");
+              getDataJson("StudentInfo");
           }}/>
 
           <Button title="Delete Data" onPress={function (){
-              removedata("name");
+              //removedata("name");
+              removedata("StudentInfo");
           }}/>
       </View>
 
